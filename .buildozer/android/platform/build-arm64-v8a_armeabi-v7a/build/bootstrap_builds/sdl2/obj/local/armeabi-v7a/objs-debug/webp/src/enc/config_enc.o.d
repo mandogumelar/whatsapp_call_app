26a7c2519cf03ddea570062dad89850aa5b82bc8
mandogumelar <mandogumelar@gmail.com>
2025-09-22 04:04:11 +0700

    Update .gitignore, remove build files

--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/webp/src/enc/config_enc.o.d
+++
@@ -1,8 +0,0 @@
-/home/mando/whatsapp_call_app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/webp/src/enc/config_enc.o: \
-  /home/mando/whatsapp_call_app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/enc/config_enc.c \
-  /home/mando/whatsapp_call_app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/encode.h \
-  /home/mando/whatsapp_call_app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/./types.h
-
-/home/mando/whatsapp_call_app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/encode.h:
-
-/home/mando/whatsapp_call_app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/./types.h:
